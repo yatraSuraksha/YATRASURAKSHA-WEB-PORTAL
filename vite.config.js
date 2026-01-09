e6142796f6842ac2958f847e27b596ac16ea3eb3
thisisharshavardhan <thisisharshavardhan@gmail.com>
2026-01-09 12:15:37 +0530

    fix: Update base path in Vite configuration to '/' and add CNAME for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/admin/',
+  base: '/',
   server: {
     port: 5173,
     strictPort: true, // Fail if port is in use instead of trying another
